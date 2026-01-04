09542dc9591e6c424291921306eedeef3b8ce2c9
Sechang Lee <binaryblitz1412@gmail.com>
2026-01-05 08:47:35 +0900

    GPT를 사용해서 자막 요약하기

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import os
 import re
-from youtube_transcript_api import YouTubeTranscriptApi
 import math
+
+from dotenv import load_dotenv
+from youtube_transcript_api import YouTubeTranscriptApi
+from openai import OpenAI
 
 def extract_video_id(url):
     pattern = r"(?:v=|\/v\/|youtu\.be\/|\/embed\/)([A-Za-z0-9_-]{11})"
@@ -48,3 +52,37 @@ for transcript in chucked_transcript:
 for content in formatted_transcript:
      print(content)
      print('--------------------------')
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+if not api_key:
+    raise RuntimeError("OPENAI_API_KEY is not set")
+
+client = OpenAI(api_key=api_key)
+
+def get_prompt(transcript):
+    return (
+        "다음은 유튜브 영상 자막 일부입니다.\n\n"
+        f"{transcript}\n\n"
+        "위 내용을 핵심 의미만 유지하여 간결하게 요약해 주세요.\n"
+        "불필요한 표현은 제거하고 핵심 정보를 구조적으로 정리하세요.\n"
+        "출력은 최대 4문장으로 제한하세요."
+    )
+
+summaried_transcript = []
+for index, transcript in enumerate(formatted_transcript):
+    prompt = get_prompt(transcript)
+
+    print(f'{index + 1}번 요약 시작')
+    response = client.responses.create(
+        model='gpt-3.5-turbo',
+        input=prompt
+    )
+    summaried_transcript.append(response.output_text)
+
+prompt = get_prompt(transcript="\n".join(summaried_transcript) + f"\n\n최종 요약은 최대 10문장으로 정리하세요.")
+final_resp = client.responses.create(
+    model='gpt-3.5-turbo',
+    input=prompt
+)
+print(final_resp.output_text)
