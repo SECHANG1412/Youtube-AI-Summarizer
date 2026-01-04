5ac4dee9c999b1e1e39e43a4eb7bc3df3cc64774
Sechang Lee <binaryblitz1412@gmail.com>
2026-01-05 07:16:43 +0900

    Youtube 영상 자막 데이터 가공하기

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
 from youtube_transcript_api import YouTubeTranscriptApi
+import math
 
 def extract_video_id(url):
     pattern = r"(?:v=|\/v\/|youtu\.be\/|\/embed\/)([A-Za-z0-9_-]{11})"
@@ -24,3 +25,26 @@ print('추출된 video_id', video_id)
 
 transcript = fetch_transcript(video_id)
 print(transcript)
+
+chunk_size = 50
+total_chunks = math.ceil(len(transcript) / chunk_size)
+
+chucked_transcript = []
+for index in range(total_chunks):
+    start_idx = index * chunk_size
+    sliced = transcript[start_idx:start_idx + chunk_size]
+    chucked_transcript.append(sliced)
+
+formatted_transcript = []
+for transcript in chucked_transcript:
+    merged_lines = []
+    for item in transcript:
+        start_time = f"{item.start:.2f}s"
+        merged_lines.append(f"{start_time}: {item.text}")
+    merged_text = "\n".join(merged_lines)
+    formatted_transcript.append(merged_text)
+
+
+for content in formatted_transcript:
+     print(content)
+     print('--------------------------')
